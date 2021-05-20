9ff4fb691a9b27a2948e78bfd67fa687963ba115
HIHYUN <khjj2121@ajou.ac.kr>
2021-05-20 17:40:13 +0900

    .

--- SanhakVR/Assets/Scripts/TriggerCubeManager.cs
+++ SanhakVR/Assets/Scripts/TriggerCubeManager.cs
@@ -6,14 +6,30 @@ using DG.Tweening;
 public class TriggerCubeManager : MonoBehaviour
 {
     public List<GameObject> signPrefabs = new List<GameObject>();
-
+    private Sequence instantMotion;
     private void Start() 
     {
+        instantMotion = DOTween.Sequence();
+        instantMotion.SetAutoKill(false);
+
+        float step = 0;
         for(int i =0; i < signPrefabs.Count; i++)
         {
             GameObject spawn = Instantiate(signPrefabs[i]);
             spawn.name = signPrefabs[i].name;
             spawn.transform.SetParent(this.transform, false);
+            instantMotion.Join(spawn.transform.DOLocalMoveX(step,2f).SetEase(Ease.OutQuint));
+            step += 1f;
+        }
+
+    }
+
+    
+    private void Update() 
+    {
+        if(this.transform.childCount ==0)
+        {
+            Debug.Log(" Clear");
         }
     }
 }
