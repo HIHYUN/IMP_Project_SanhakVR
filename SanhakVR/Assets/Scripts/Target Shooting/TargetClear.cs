ff44e7af85b5724faced789afc1c34d557b8e754
masaywar <dngus0715@naver.com>
2021-06-06 21:35:42 +0900

    Shooting Target Update

--- SanhakVR/Assets/Scripts/Target Shooting/TargetClear.cs
+++ SanhakVR/Assets/Scripts/Target Shooting/TargetClear.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TargetClear : MonoBehaviour
+public class TargetClear : MonoBehaviour, IPuzzle
 {
     public int clearCount;
-    public GameObject clearText;
-    
+
+    private bool isSolved = false;
+    private Stage stage;
+
+    private void Start()
+    {
+        stage = StageController.Instance.activeStage;
+        stage.addPuzzle(this);
+    }
+
+    public void CheckSolved()
+    {
+        if (clearCount == 3)
+        {
+            isSolved = true;
+        }
+    }
+
+    public bool IsSolved()
+    {
+        return isSolved;
+    }
+
+
+
+
     // Update is called once per frame
     void Update()
     {
-        if(clearCount ==3)
-        {
-            clearText.SetActive(true);
-        }
+        CheckSolved();
     }
 }
--- SanhakVR/Assets/Scripts/Target Shooting/TargetController.cs
+++ SanhakVR/Assets/Scripts/Target Shooting/TargetController.cs
@@ -3,37 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class TargetController : MonoBehaviour, IPuzzle
+public class TargetController : MonoBehaviour
 {
     public int count = 0;
-    private Stage stage;
-    private bool isSolved = false;
 
-
-
-    private void Start()
-    {
-        stage = StageController.Instance.activeStage;
-        stage.addPuzzle(this);
-    }
-
-    public void CheckSolved()
+    private void Update() 
     {
         if (count > 10)
         {
             transform.DORotate(new Vector3(-90, 0, 0), 3f, RotateMode.WorldAxisAdd);
             count = 0;
         }
-    }
-
-    public bool IsSolved()
-    {
-        return isSolved;
-    }
-
-    private void Update() 
-    {
-        
     }
 
 
